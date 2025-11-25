f0806d0396e4d436f2297fe0275ea3606fef244b
pellekrogsgaard-ops <pelle.krogsgaard@gmail.com>
2025-11-25 23:25:56 +0100

    HealthSystem
    Scripts til health og klargøring af UI

---
+++ Assets/Scripts/Health System/EnemyDamage.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    public PlayerHealth playerHealth;
+    public int damage = 20;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Collided with Player");
+            playerHealth.TakeDamage(damage);
+        }
+    }
+}
---
+++ Assets/Scripts/Health System/PlayerHealth.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int health;
+    public int maxHealth = 100;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+ public void TakeDamage(int amount)
+    {
+        health -= amount;
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+    void Die()
+    {
+        // Handle player death (e.g., play animation, restart level, etc.)
+        Debug.Log("Player has died.");
+    }
+
+
+
+
+}
