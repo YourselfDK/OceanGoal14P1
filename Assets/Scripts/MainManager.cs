a98d3b50414285ee1bc69a1f24ba376bf6c83dd7
YourselfDK <johan.bergmann@hotmail.com>
2025-11-28 11:21:02 +0100

    Fixed Health bar

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -1,9 +1,13 @@
+using TMPro;
 using UnityEngine;
 
 public class MainManager : MonoBehaviour
 {
 
     public static MainManager Instance;
+    public float HealthCount;
+    private int CoinCount;
+    public TextMeshProUGUI coinText;
 
     private void Awake()
     {
@@ -14,5 +18,22 @@ public class MainManager : MonoBehaviour
         }
         Instance = this;
         DontDestroyOnLoad(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.transform.tag == "Coin")
+        {
+
+
+            CoinCount++;
+            coinText.text = "Coins:" + CoinCount.ToString();
+            Debug.Log(CoinCount);
+            Destroy(other.gameObject);
+
+
+        }
+
     }
 }
