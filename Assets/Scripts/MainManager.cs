870e23f113d36b3a0ba8c60ccc46e31d6bf321e7
YourselfDK <johan.bergmann@hotmail.com>
2025-12-01 15:15:09 +0100

    Update MainManager.cs

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -7,7 +7,7 @@ public class MainManager : MonoBehaviour
     public static MainManager Instance;
     public int HealthCount;
     public int CoinCount;
-    public int LevelCount = 1;
+    public int LevelCount;
     public bool InShop = false;
 
     private void Awake()
