b46ef83618667dfc90965a5bacb29cbd4cada809
OliverRS <oliversegato2@gmail.com>
2025-12-01 13:41:14 +0100

    EnemyColliderFIx

--- Assets/Scripts/Health System/EnemyDamage.cs
+++ Assets/Scripts/Health System/EnemyDamage.cs
@@ -18,11 +18,11 @@ public class EnemyDamage : MonoBehaviour
         
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Player")
         {
-            Debug.Log("Collided with Player");
+            Debug.Log("Triggered with Player");
             playerHealth.TakeDamage(damage);
         }
     }
