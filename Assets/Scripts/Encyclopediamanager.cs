2c00e981cec70e75997df6dbb9e8d90ae48d1c18
leobeo <leocarvalheiro@gmail.com>
2025-12-02 15:13:57 +0100

    Error Encyclopedia

---
+++ Assets/Scripts/Encyclopediamanager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EncyclopediaNavigation : MonoBehaviour
+{
+     
+    public GameObject FishPanel; 
+    public GameObject HazardsPanel; 
+    public GameObject SolutionPanels;   
+
+    public void ChangeThat (string nameOfPanel)
+    {
+        FishPanel.SetActive(nameOfPanel == "FishPanel");
+        HazardsPanel.SetActive(nameOfPanel == "HazardsPanel");
+        SolutionPanels.SetActive(nameOfPanel == "SolutionsPanel");
+    }
+}
