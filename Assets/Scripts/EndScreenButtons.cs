899a7ee392d7c62143935ed0587c60434d9a8ad7
Kristina <kristina.k.d@live.dk>
2025-12-08 12:18:36 +0100

    level 6 done

--- Assets/Scripts/EndScreenButtons.cs
+++ Assets/Scripts/EndScreenButtons.cs
@@ -3,12 +3,17 @@ using UnityEngine.SceneManagement;
 
 public class EndScreenButtons : MonoBehaviour
 {
-    public string mainMenuScene = "Main Menu";
-
     public void LoadMenu()
     {
         Time.timeScale = 1f;
-        SceneManager.LoadScene("Main Menu");
+        SceneManager.LoadScene(0);
+        MainManager.Instance.CoinCount = 0;
+        MainManager.Instance.LevelCount = 1;
+        MainManager.Instance.FisherCleanup = 0;
+        MainManager.Instance.ODeprivedCleanup = 0;
+        MainManager.Instance.MPlasticCleanup = 0;
+        MainManager.Instance.ODeprivedCleanup = 0;
+        MainManager.Instance.ToxicWasteCleanup = 0;
     }
 
     public void QuitGame()
